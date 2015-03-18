9235566f0ba8bca47b5fc0cf1525aed74df657e8
rrichar1 <r.remy4@gmail.com>
2015-03-18 11:56:38 +0100

    AvatarDatabaseSeeder ajout

--- app/database/seeds/AvatarDatabaseSeeder.php
+++ app/database/seeds/AvatarDatabaseSeeder.php
@@ -9,13 +9,13 @@ class AvatarDatabaseSeeder extends Seeder {
 
             Avatar::create(array(
                             'email' => 'r.remy4@gmail.com',
-                            'photo' => 'pictures/img.png',
+                            'photo' => 'pictures/img.jpg',
                             'user_id' => 1
                         ));
             
             Avatar::create(array(
                             'email' => 'r.remy@gmail.com',
-                            'photo' => 'pictures/img2.png',
+                            'photo' => 'pictures/img2.jpg',
                             'user_id' => 1
                         ));
 	}
