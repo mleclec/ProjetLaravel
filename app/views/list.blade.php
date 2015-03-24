5fb732eb7471a5b52e6e073a268737ae9fbbae78
rrichar1 <r.remy4@gmail.com>
2015-03-24 23:01:20 +0100

    ajout confirmation de supp avatar

--- app/views/list.blade.php
+++ app/views/list.blade.php
@@ -1,4 +1,14 @@
 @extends('layout.master')
+<script language='Javascript'>
+    function verifForm(f){
+        var del = confirm('Confirmer la suppression ?');
+        if(del == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+</script>
 
 @section('header')
         <nav class="navbar navbar-default">
@@ -21,12 +31,13 @@
         @foreach($avatars as $a)
         <div class="col-sm-6 col-md-4">
             <div class="thumbnail">
-                {{ HTML::image($a->photo) }}
+                {{ HTML::image($a->photo, 'image de l\'avatar', array('width' => 242,'height' => 200)) }}
                 <div class="caption">
                     <h3>{{ $a->email }}</h3>
                     {{ Form::open(array(
                                 'action'=> 'ProfileController@deleteAvatar',
-                                'method'=>'POST')) }}
+                                'method'=>'POST',
+                                'onsubmit' => 'return verifForm(this)')) }}
             
                             {{ Form::hidden('id', $a->id) }}
                             {{ Form::hidden('user', $user) }}
